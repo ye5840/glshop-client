59204297888239466f055485d1d0a56a4d1c297e
linzhen5840 <584029578@qq.com>
2020-07-11 16:17:52 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>123</div>
+  <div>
+    <Header></Header>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
   name: '',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
